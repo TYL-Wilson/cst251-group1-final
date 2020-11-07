20a1bed77cec4f87da6ba86878d5cd843705c868
mgubanova <70294900+mgubanova@users.noreply.github.com>
2020-11-07 10:31:10 -0800

    doorswork

--- js/door.js
+++ js/door.js
@@ -1,6 +1,85 @@
-var element = document.querySelector(".door");
-element.addEventListener("click", toggleDoor);
+var element1 = document.querySelector(".door");
+element1.addEventListener("click", toggleDoor);
 
 function toggleDoor() {
-  element.classList.toggle("doorOpen");
+  element1.classList.toggle("doorOpen");
 }
+
+var element2 = document.querySelector(".door2");
+element2.addEventListener("click", toggleDoor2);
+
+function toggleDoor2() {
+  element2.classList.toggle("doorOpen2");
+}
+
+var element3 = document.querySelector(".door3");
+element3.addEventListener("click", toggleDoor3);
+
+function toggleDoor3() {
+  element3.classList.toggle("doorOpen3");
+}
+
+var element4 = document.querySelector(".door4");
+element4.addEventListener("click", toggleDoor4);
+
+function toggleDoor4() {
+  element4.classList.toggle("doorOpen4");
+}
+
+var element5 = document.querySelector(".door5");
+element5.addEventListener("click", toggleDoor5);
+
+function toggleDoor5() {
+  element5.classList.toggle("doorOpen5");
+}
+
+var element6 = document.querySelector(".door6");
+element6.addEventListener("click", toggleDoor6);
+
+function toggleDoor6() {
+  element6.classList.toggle("doorOpen3");
+}
+
+var element7 = document.querySelector(".door7");
+element7.addEventListener("click", toggleDoor7);
+
+function toggleDoor7() {
+  element7.classList.toggle("doorOpen7");
+}
+
+var element8 = document.querySelector(".door8");
+element8.addEventListener("click", toggleDoor8);
+
+function toggleDoor8() {
+  element8.classList.toggle("doorOpen8");
+}
+
+var element9 = document.querySelector(".door9");
+element9.addEventListener("click", toggleDoor9);
+
+function toggleDoor9() {
+  element9.classList.toggle("doorOpen9");
+}
+
+var element10 = document.querySelector(".door10");
+element10.addEventListener("click", toggleDoor10);
+
+function toggleDoor10() {
+  element10.classList.toggle("doorOpen10");
+}
+
+var element11 = document.querySelector(".door11");
+element11.addEventListener("click", toggleDoor11);
+
+function toggleDoor11() {
+  element11.classList.toggle("doorOpen11");
+}
+
+var element12 = document.querySelector(".door12");
+element12.addEventListener("click", toggleDoor12);
+
+function toggleDoor12() {
+  element12.classList.toggle("doorOpen12");
+}
+//var element = document.querySelector(".door2");
+//element.addEventListener("click", toggleDoor);
